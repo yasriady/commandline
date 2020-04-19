cd262d94a7b3e462ab25b4976cf52d96ba502a0b
Dedy Yasriady <yasriady@yahoo.com>
2020-04-19 13:20:01 +0700

    Create customize_fail2ban.sh

---
+++ customize_fail2ban.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Fail2Ban configuration file
+#
+# Save attacker (brute force) ip address into database server
+#
+# Author: Dedy Yasriady (yasriady@gmail.com)
+# Date: Pekanbaru (Riau) Indonesia, 2020-Apr-19
+# Modified by Dedy Yasriady by adding time, failures, servername (hostname)
+#
+#sudo scp -P9722 dedy@srvutils.pekanbaru.go.id:/var/www/blog.pekanbaru.go.id/wip/saveip.conf ./
+echo "Hello Daffa dan Azzam !"
+
